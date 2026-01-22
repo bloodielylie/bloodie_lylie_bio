4232456a4f8a32c145e3a8b40e684310278dca28
bloodielylie <hoangngockhanhly@gmail.com>
2026-01-22 12:23:38 +0700

    Update products.js

--- products.js
+++ products.js
@@ -5,6 +5,11 @@ const products = [
     name: "Thảm để bàn học sinh Thảm trải bàn máy tính bằng da"
   },
   {
+    link: "https://s.shopee.vn/2g54peDBVQ",
+    image: "https://down-vn.img.susercontent.com/file/cn-11134207-7ras8-m33r71swxxxe08.webp",
+    name: "Thảm trải bàn chống dầu"
+  },
+  {
     link: "https://s.shopee.vn/xxxxxx",
     image: "assets/product2.jpg",
     name: "Bàn phím cơ gaming"
@@ -25,3 +30,4 @@ const products = [
 
 
 ];
+
