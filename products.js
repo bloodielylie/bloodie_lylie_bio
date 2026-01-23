419370f2dbf71fa2cc2dd1430874cab05b4e163f
bloodielylie <hoangngockhanhly@gmail.com>
2026-01-23 15:02:24 +0700

    Update products.js

--- products.js
+++ products.js
@@ -5,9 +5,9 @@ const products = [
     name: "Thảm trải bàn làm việc da PU"
   },
   {
-    link: "https://s.shopee.vn/2g54peDBVQ",
-    image: "https://down-vn.img.susercontent.com/file/cn-11134207-7ras8-m33r71swxxxe08.webp",
-    name: "Thảm trải bàn làm việc da PU"
+    link: "https://s.shopee.vn/4q9boIGyWm",
+    image: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lvxk10fby19761.webp",
+    name: "Cặp kẹp bàn pegboard bằng kim loại"
   },
   {
     link: "https://s.shopee.vn/2g54peDBVQ",
@@ -49,3 +49,4 @@ const products = [
 
 
 ];
+
